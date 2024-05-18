73f447d58c3f680b2cbc4f9c8e708051329ba016
JakubSzczegielniak <jakub.szczegielniak@hotmail.com>
2024-05-18 12:59:07 +0200

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My application for Universtity.
         </p>
         <a
           className="App-link"
